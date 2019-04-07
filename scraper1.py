b808d4c8950425922eb4c18c432b5163c94dd089
Arjun Ramani <arjunramani3@gmail.com>
2019-04-06 22:03:33 -0700

    integrate amazon s3 upload

--- scraper1.py
+++ scraper1.py
@@ -1,6 +1,14 @@
 #Try out scraping functionality
 
+
+import boto3
+
+
+# Create an S3 client
+s3 = boto3.client('s3')
+
 from tabula import read_pdf
 
 df = read_pdf('https://irds.stanford.edu/sites/g/files/sbiybj10071/f/5.2_endowment.pdf')
-print(df)
+df.to_csv('endowment.csv')
+s3.upload_file('endowment.csv', 'open-data-portal', 'endowment.csv', ExtraArgs={'ContentType': "text/csv", 'ACL':'public-read'})
