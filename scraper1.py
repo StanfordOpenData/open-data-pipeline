4a4f08d4dec98c94f157693b858ed68a60d97983
Arjun Ramani <arjunramani3@gmail.com>
2019-04-23 00:52:11 +0100

    added loop to single-table pdf

--- scraper1.py
+++ scraper1.py
@@ -1,5 +1,6 @@
-#Try out scraping functionality
-
+#Upload data to s3 for download from the ODP
+##Data Types: Single-table Pdf, Multi-table Pdf, CSV, HTML
+#This file is for single-table pdf's
 
 import boto3
 
@@ -9,6 +10,12 @@ s3 = boto3.client('s3')
 
 from tabula import read_pdf
 
-df = read_pdf('https://irds.stanford.edu/sites/g/files/sbiybj10071/f/5.2_endowment.pdf')
-df.to_csv('endowment.csv')
-s3.upload_file('endowment.csv', 'open-data-portal', 'endowment.csv', ExtraArgs={'ContentType': "text/csv", 'ACL':'public-read'})
+endowment = ['endowment.csv','https://irds.stanford.edu/sites/g/files/sbiybj10071/f/5.2_endowment.pdf']
+fac_adm = ['facilities_administration.csv', 'https://irds.stanford.edu/sites/default/files/5.6_facilities-administrative_cost_rates.pdf']
+pdfs = [endowment, fac_adm]
+
+for data in pdfs:
+	name = data[0]; url = data[1];
+	df = read_pdf(url)
+	df.to_csv(name)
+	s3.upload_file(name, 'open-data-portal', name, ExtraArgs={'ContentType': "text/csv", 'ACL':'public-read'})
